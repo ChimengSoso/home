03f74d2a2f2295eb744c263de060b2db43bfd6de
Worakun Ata <chi.cm@traditionasia.com>
2022-08-19 17:48:30 +0700

    routing home and donate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css';
 import React from 'react';
+import {Routes, Route} from "react-router-dom";
 import {Home} from "./component/home/Home";
 import {Donate} from "./component/donation/Donate";
 
 type Prop = {}
 
 const App: React.FC<Prop> = () => {
-    return (<Donate/>);
+    return (<>
+        <Routes>
+            <Route path={"/cm"} element={<Home/>}/>
+            <Route path={"/donate"} element={<Donate/>}/>
+        </Routes>
+    </>);
 }
 
 export default App;
